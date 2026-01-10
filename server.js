d0958f551dd1af38530f2033297863c96af35ea6
Mayookh <mayookh@beatly.ai>
2026-01-10 14:42:15 +0530

    updated two card design updated index page also

--- server.js
+++ server.js
@@ -88,13 +88,12 @@ app.post(
       const existingPdfBytes = fs.readFileSync(pdfPath);
       const originalPdf = await PDFDocument.load(existingPdfBytes);
 
-
       let removePages = [];
       if (req.body.page) {
         removePages = req.body.page
           .split(",")
-          .map(p => parseInt(p.trim(), 10) - 1)
-          .filter(p => !isNaN(p));
+          .map((p) => parseInt(p.trim(), 10) - 1)
+          .filter((p) => !isNaN(p));
       }
 
       const pdfDoc = await PDFDocument.create();
@@ -112,7 +111,7 @@ app.post(
       }
 
       const copiedPages = await pdfDoc.copyPages(originalPdf, pagesToKeep);
-      copiedPages.forEach(p => pdfDoc.addPage(p));
+      copiedPages.forEach((p) => pdfDoc.addPage(p));
 
       const defaultLogoBytes = fs.readFileSync(
         path.join(__dirname, "assets/default_logo.png")
@@ -124,86 +123,120 @@ app.post(
         const logoBytes = fs.readFileSync(hospitalLogoFile.path);
         hospitalLogoImage = await pdfDoc.embedPng(logoBytes);
       }
-
-      const {
-        textName,
-        HospitalName,
-        education,
-        refDoctor,
-        serialNo,
-      } = req.body;
-
+      const { mode, textName, HospitalName, education, refDoctor, serialNo } =
+        req.body;
       const pages = pdfDoc.getPages();
-
       pages.forEach((page, index) => {
         const { width, height } = page.getSize();
 
-        const leftDims = defaultLogoImage.scaleToFit(100, 100);
-        page.drawImage(defaultLogoImage, {
-          x: 65,
-          y: height - leftDims.height - 20,
-          width: leftDims.width,
-          height: leftDims.height,
-        });
-
-        if (hospitalLogoImage) {
-          const dims = hospitalLogoImage.scaleToFit(60, 60);
-          const rightX = width - dims.width - 100;
-          const topY = height - 10;
-
-          page.drawImage(hospitalLogoImage, {
-            x: rightX,
-            y: topY - dims.height,
-            width: dims.width,
-            height: dims.height,
+        if (mode.includes("rx")) {
+          const leftDims = defaultLogoImage.scaleToFit(100, 100);
+          page.drawImage(defaultLogoImage, {
+            x: 65,
+            y: height - leftDims.height - 20,
+            width: leftDims.width,
+            height: leftDims.height,
           });
 
-          let y = topY - dims.height - 20;
-          if (textName) {
-            page.drawText(textName, { x: rightX, y, size: 11 });
-            y -= 15;
-          }
-          if (education) {
-            page.drawText(education, { x: rightX, y, size: 9 });
-          }
-        } else {
-          let y = height - 50;
-          const x = width - 200;
+          if (hospitalLogoImage) {
+            const dims = hospitalLogoImage.scaleToFit(60, 60);
+            const rightX = width - dims.width - 100;
+            const topY = height - 10;
 
-          if (HospitalName) {
-            page.drawText(HospitalName, { x, y, size: 12 });
-            y -= 15;
-          }
-          if (textName) {
-            page.drawText(textName, { x, y, size: 11 });
-            y -= 15;
-          }
-          if (education) {
-            page.drawText(education, { x, y, size: 9 });
-          }
-        }
+            page.drawImage(hospitalLogoImage, {
+              x: rightX,
+              y: topY - dims.height,
+              width: dims.width,
+              height: dims.height,
+            });
 
-        if (index === 0) {
-          if (refDoctor) {
-            page.drawText(`Referral Doctor ${refDoctor}`, {
-              x: 72,
+            let y = topY - dims.height - 20;
+            if (textName) {
+              page.drawText(textName, { x: rightX, y, size: 11 });
+              y -= 15;
+            }
+            if (education) {
+              page.drawText(education, { x: rightX, y, size: 9 });
+            }
+          } else {
+            let y = height - 50;
+            const x = width - 200;
+
+            if (HospitalName) {
+              page.drawText(HospitalName, { x, y, size: 12 });
+              y -= 15;
+            }
+            if (textName) {
+              page.drawText(textName, { x, y, size: 11 });
+              y -= 15;
+            }
+            if (education) {
+              page.drawText(education, { x, y, size: 9 });
+            }
+          }
+
+          if (index === 0) {
+            if (refDoctor) {
+              page.drawText(`Referral Doctor ${refDoctor}`, {
+                x: 72,
+                y: height - 211,
+                size: 10,
+              });
+            }
+
+            page.drawText("BeatX Lite", {
+              x: 385,
               y: height - 211,
               size: 10,
             });
+
+            if (serialNo) {
+              page.drawText(serialNo, {
+                x: 385,
+                y: height - 225,
+                size: 10,
+              });
+            }
           }
+        } else {
+          if (index === 0 || index == 1) {
+            if (hospitalLogoImage) {
+              const dims = hospitalLogoImage.scaleToFit(60, 60);
+              const rightX = width - dims.width - 150;
+              const topY = height;
 
-          page.drawText("BeatX Lite", {
-            x: 385,
-            y: height - 211,
-            size: 10,
-          });
+              page.drawImage(hospitalLogoImage, {
+                x: rightX + 10,
+                y: topY - dims.height - 5,
+                width: dims.width,
+                height: dims.height,
+              });
 
-          if (serialNo) {
-            page.drawText(serialNo, {
-              x: 385,
-              y: height - 225,
-              size: 10,
-            });
+              let y = topY - 30;
+
+              if (textName) {
+                page.drawText(textName, { x: rightX + 70, y, size: 9 });
+                y -= 15;
+              }
+              if (education) {
+                page.drawText(education, { x: rightX + 80, y, size: 9 });
+              }
+            } else {
+              let y = height - 25;
+              const x = width - 200;
+
+              if (HospitalName) {
+                page.drawText(HospitalName, { x, y, size: 12 });
+                y -= 15;
+              }
+              if (textName) {
+                page.drawText(textName, { x, y, size: 9 });
+                y -= 15;
+              }
+              if (education) {
+                page.drawText(education, { x, y, size: 9 });
+              }
+            }
           }
         }
       });
@@ -218,7 +251,6 @@ app.post(
         safeDelete(uploadedPdf.path);
         safeDelete(hospitalLogoFile?.path);
       });
-
     } catch (err) {
       console.error("Processing error:", err);
 
@@ -226,7 +258,10 @@ app.post(
       safeDelete(uploadedPdf?.path);
       safeDelete(hospitalLogoFile?.path);
 
-      req.flash("error", err.message || "Something went wrong while processing the PDF");
+      req.flash(
+        "error",
+        err.message || "Something went wrong while processing the PDF"
+      );
       return res.render("index");
     }
   }
